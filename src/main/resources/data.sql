a7bd89facd33491e0729f899b1b1f0768fbaafe5
leewooseok <as05086@naver.com>
2023-05-17 17:50:40 +0900

    second commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
 INSERT INTO article(id,title,content) VALUES(1,'가가가가','1111');
 INSERT INTO article(id,title,content) VALUES(2,'나나나나','2222');
 INSERT INTO article(id,title,content) VALUES(3,'다다다다','3333');
+INSERT INTO article(id,title,content) VALUES(4,'다다dd다다','3333');
